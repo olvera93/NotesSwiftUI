45399bc99ec6c06b64c9b928a22b1acd51f1a03f
olvera93 <olvera9302@outlook.com>
2023-01-14 19:17:34 -0600

    [ADD] Show notes in a list

--- Notes/View/Home.swift
+++ Notes/View/Home.swift
@@ -10,15 +10,30 @@ import SwiftUI
 struct Home: View {
     
     @StateObject var model = ViewModel()
+    @FetchRequest(entity: Notes.entity(), sortDescriptors: [NSSortDescriptor(key: "date", ascending: true)], animation: .spring()) var results : FetchedResults<Notes>
     
     var body: some View {
-        Button(action: {
-            model.show.toggle()
-        }) {
-            Text("+")
-        }.sheet(isPresented: $model.show, content: {
-            addView(model: model)
-        })
+        NavigationView() {
+            List {
+                ForEach(results) { item in
+                    VStack(alignment: .leading) {
+                        Text(item.note ?? "")
+                            .font(.title)
+                            .bold()
+                        
+                        Text(item.date ?? Date(), style: .date)
+                    }
+                }
+            }.navigationTitle("Notes")
+                .navigationBarItems(trailing: Button(action: {
+                    model.show.toggle()
+                }) {
+                    Image(systemName: "plus").font(.title).foregroundColor(.blue)
+                }
+                ).sheet(isPresented: $model.show, content: {
+                    addView(model: model)
+                })
+        }
     }
 }
 
